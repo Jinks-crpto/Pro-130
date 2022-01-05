9259110ea75b3b00fbe89e5f28d126585b564878
Jinks-crpto <77697885+Jinks-crpto@users.noreply.github.com>
2022-01-04 19:29:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function room(){
+    window.location="bedroom.html"
+}
+function room1(){
+    window.location="basement.html"
+}
+function room2(){
+    window.location="closet.html"
+}
+function room3(){
+    window.location="livingroom.html"
+}
+function room4(){
+    window.location="hallway.html"
+}
